7058d75213d9665cbb6d02664d6328f94faaab37
prataprc <prataprc@gmail.com>
2020-10-29 19:37:37 +0530

    perf: performance tool for Vector and im::Vector.

---
+++ src/bin/perf.rs
@@ -0,0 +1,225 @@
+use im;
+use rand::{prelude::random, rngs::SmallRng, Rng, SeedableRng};
+use structopt::StructOpt;
+
+use std::time;
+
+use ppar;
+
+// NOTE: im::Vector does not remove/delete op.
+// NOTE: im::Vector, how to measure the value footprint.
+
+#[macro_export]
+macro_rules! pp {
+    ($($arg:expr),+ => $val:expr) => {
+        println!("{:<30} : {:?}", format!($($arg),+), $val)
+    };
+}
+
+/// Command line options.
+#[derive(Clone, StructOpt)]
+pub struct Opt {
+    #[structopt(long = "seed")]
+    seed: Option<u128>,
+
+    #[structopt(long = "load", default_value = "1000000")] // default 1M
+    load: usize,
+
+    #[structopt(long = "ops", default_value = "1000000")] // default 1M
+    ops: usize,
+
+    #[structopt(long = "leaf-size")] // default 1M
+    leaf_size: Option<usize>,
+}
+
+fn main() {
+    let mut opts = Opt::from_args();
+    let mut rng = {
+        let seed = opts.seed.unwrap_or(random());
+        // let seed: u128 = 89704735013013664095413923566273445973;
+        SmallRng::from_seed(seed.to_le_bytes())
+    };
+
+    println!("\nppar::Vector performance characterization");
+    println!("-----------------------------------------");
+    let mut arr: ppar::Vector<u64> = ppar::Vector::new();
+    opts.leaf_size.map(|s| arr.set_leaf_size(s));
+    let mut arr = ppar_load(arr, &mut opts, &mut rng);
+    arr = ppar_ops(arr, &mut opts, &mut rng);
+    ppar_delete_skew(arr, &mut rng);
+
+    println!("\nim::Vector performance characterization");
+    println!("---------------------------------------\n");
+    let arr = im_load(&mut opts, &mut rng);
+    im_ops(arr, &mut opts, &mut rng);
+}
+
+fn mem_ratio(mem: usize, n: usize) -> f64 {
+    let s = 8;
+    ((((mem as f64) / (n as f64)) - (s as f64)) / s as f64) * 100_f64
+}
+
+fn ppar_load(mut arr: ppar::Vector<u64>, opts: &mut Opt, rng: &mut SmallRng) -> ppar::Vector<u64> {
+    let vals: Vec<u64> = (0..opts.load).map(|_| rng.gen()).collect();
+    let offs: Vec<usize> = (0..opts.load)
+        .map(|n| rng.gen::<usize>() % (n + 1))
+        .collect();
+
+    {
+        let start = time::Instant::now();
+        let arr = ppar::Vector::from_slice(&vals, opts.leaf_size);
+        pp!(
+            "append-load({} items)",
+            arr.len()
+            =>
+            start.elapsed() / (opts.load as u32)
+        );
+    }
+
+    {
+        let (offs, vals) = (offs.clone(), vals.clone());
+        let start = time::Instant::now();
+        for (off, val) in offs.into_iter().zip(vals) {
+            arr = arr.insert(off, val).unwrap();
+        }
+        pp!(
+            "random-load({} items)",
+            arr.len()
+            =>
+            start.elapsed() / (opts.load as u32)
+        );
+    }
+
+    arr
+}
+
+fn ppar_ops(mut arr: ppar::Vector<u64>, opts: &Opt, rng: &mut SmallRng) -> ppar::Vector<u64> {
+    let vals: Vec<u64> = (0..opts.ops).map(|_| rng.gen()).collect();
+    let offs: Vec<usize> = (0..opts.ops)
+        .map(|_| rng.gen::<usize>() % arr.len())
+        .collect();
+
+    {
+        let start = time::Instant::now();
+        let offs = offs.clone();
+        for off in offs.into_iter() {
+            arr.get(off).unwrap();
+        }
+        pp!(
+            "get({} ops)",
+            opts.ops
+            =>
+            start.elapsed() / (opts.ops as u32)
+        );
+    }
+
+    {
+        let start = time::Instant::now();
+        let offs = offs.clone();
+        let vals = vals.clone();
+        for (off, val) in offs.into_iter().zip(vals.into_iter()) {
+            arr = arr.set(off, val).unwrap();
+        }
+        pp!(
+            "set({} ops)",
+            opts.ops
+            =>
+            start.elapsed() / (opts.ops as u32)
+        );
+    }
+
+    {
+        let start = time::Instant::now();
+        let offs = offs.clone();
+        let vals = vals.clone();
+        for (off, val) in offs.into_iter().zip(vals.into_iter()) {
+            arr = arr.delete(off).unwrap();
+            arr = arr.insert(off, val).unwrap();
+        }
+        pp!(
+            "delete-insert({} ops)",
+            opts.ops
+            =>
+            start.elapsed() / (opts.ops as u32)
+        );
+    }
+
+    let ratio = format!("{:.2}%", mem_ratio(arr.footprint(), arr.len()));
+    pp!("overhead" => ratio);
+
+    arr
+}
+
+fn ppar_delete_skew(mut arr: ppar::Vector<u64>, rng: &mut SmallRng) -> ppar::Vector<u64> {
+    let offs: Vec<usize> = (0..((arr.len() / 10) * 9))
+        .map(|n| rng.gen::<usize>() % (arr.len() - n))
+        .collect();
+
+    for off in offs.into_iter() {
+        arr = arr.delete(off).unwrap();
+    }
+
+    let ratio = format!("{:.2}%", mem_ratio(arr.footprint(), arr.len()));
+    pp!("overhead after 90% delete" => ratio);
+
+    arr
+}
+
+fn im_load(opts: &mut Opt, rng: &mut SmallRng) -> im::Vector<u64> {
+    let vals: Vec<u64> = (0..opts.load).map(|_| rng.gen()).collect();
+
+    let arr = {
+        let mut arr: im::Vector<u64> = im::Vector::new();
+        let start = time::Instant::now();
+        for val in vals.into_iter() {
+            arr.push_back(val);
+        }
+        pp!(
+            "append-load({} items)",
+            arr.len()
+            =>
+            start.elapsed() / (opts.load as u32)
+        );
+        arr
+    };
+
+    arr
+}
+
+fn im_ops(mut arr: im::Vector<u64>, opts: &Opt, rng: &mut SmallRng) -> im::Vector<u64> {
+    let vals: Vec<u64> = (0..opts.ops).map(|_| rng.gen()).collect();
+    let offs: Vec<usize> = (0..opts.ops)
+        .map(|_| rng.gen::<usize>() % arr.len())
+        .collect();
+
+    {
+        let start = time::Instant::now();
+        let offs = offs.clone();
+        for off in offs.into_iter() {
+            arr.get(off).unwrap();
+        }
+        pp!(
+            "get({} ops)",
+            opts.ops
+            =>
+            start.elapsed() / (opts.ops as u32)
+        );
+    }
+
+    {
+        let start = time::Instant::now();
+        let offs = offs.clone();
+        let vals = vals.clone();
+        for (off, val) in offs.into_iter().zip(vals.into_iter()) {
+            arr = arr.update(off, val);
+        }
+        pp!(
+            "update({} ops)",
+            opts.ops
+            =>
+            start.elapsed() / (opts.ops as u32)
+        );
+    }
+
+    arr
+}
