efb122707ddea84d2ac8bcff47cf9785ac625f6f
prataprc <prataprc@gmail.com>
2020-10-31 09:49:45 +0530

    benchmark clone().

--- benches/ppar.rs
+++ benches/ppar.rs
@@ -11,26 +11,26 @@ use ppar::Vector;
 fn bench_prepend(b: &mut Bencher) {
     let seed: u128 = random();
     let mut rng = SmallRng::from_seed(seed.to_le_bytes());
-    let mut rr: Vector<u64> = Vector::new();
+    let mut arr: Vector<u64> = Vector::new();
     b.iter(|| {
-        rr = rr.insert(0, rng.gen::<u64>()).unwrap();
+        arr = arr.insert(0, rng.gen::<u64>()).unwrap();
     });
 
-    let ratio = mem_ratio(8, rr.footprint(), rr.len());
-    println!("bench_prepend n:{} mem_ratio:{}%", rr.len(), ratio);
+    let ratio = mem_ratio(8, arr.footprint(), arr.len());
+    println!("bench_prepend n:{} mem_ratio:{}%", arr.len(), ratio);
 }
 
 #[bench]
 fn bench_append(b: &mut Bencher) {
     let seed: u128 = random();
     let mut rng = SmallRng::from_seed(seed.to_le_bytes());
-    let mut rr: Vector<u64> = Vector::new();
+    let mut arr: Vector<u64> = Vector::new();
     b.iter(|| {
-        rr = rr.insert(rr.len(), rng.gen::<u64>()).unwrap();
+        arr = arr.insert(arr.len(), rng.gen::<u64>()).unwrap();
     });
 
-    let ratio = mem_ratio(8, rr.footprint(), rr.len());
-    println!("bench_append n:{} mem_ratio:{}%", rr.len(), ratio);
+    let ratio = mem_ratio(8, arr.footprint(), arr.len());
+    println!("bench_append n:{} mem_ratio:{}%", arr.len(), ratio);
 }
 
 #[bench]
@@ -38,14 +38,14 @@ fn bench_insert_rand(b: &mut Bencher) {
     let seed: u128 = random();
     let mut rng = SmallRng::from_seed(seed.to_le_bytes());
 
-    let mut rr: Vector<u64> = Vector::new();
+    let mut arr: Vector<u64> = Vector::new();
     b.iter(|| {
-        let off = rng.gen::<usize>() % (rr.len() + 1);
-        rr = rr.insert(off, rng.gen::<u64>()).unwrap();
+        let off = rng.gen::<usize>() % (arr.len() + 1);
+        arr = arr.insert(off, rng.gen::<u64>()).unwrap();
     });
 
-    let ratio = mem_ratio(8, rr.footprint(), rr.len());
-    println!("bench_rand n:{} mem_ratio:{}%", rr.len(), ratio);
+    let ratio = mem_ratio(8, arr.footprint(), arr.len());
+    println!("bench_rand n:{} mem_ratio:{}%", arr.len(), ratio);
 }
 
 #[bench]
@@ -53,18 +53,18 @@ fn bench_insert_rand(b: &mut Bencher) {
 fn bench_get_100K(b: &mut Bencher) {
     let seed: u128 = random();
     let mut rng = SmallRng::from_seed(seed.to_le_bytes());
-    let mut rr: Vector<u64> = Vector::new();
+    let mut arr: Vector<u64> = Vector::new();
     for _ in 0..100_000 {
-        let off = rng.gen::<usize>() % (rr.len() + 1);
-        rr = rr.insert(off, rng.gen::<u64>()).unwrap();
+        let off = rng.gen::<usize>() % (arr.len() + 1);
+        arr = arr.insert(off, rng.gen::<u64>()).unwrap();
     }
     b.iter(|| {
-        let off = rng.gen::<usize>() % rr.len();
-        rr.get(off).unwrap();
+        let off = rng.gen::<usize>() % arr.len();
+        arr.get(off).unwrap();
     });
 
-    let ratio = mem_ratio(8, rr.footprint(), rr.len());
-    println!("bench_get_100K n:{} mem_ratio:{}%", rr.len(), ratio);
+    let ratio = mem_ratio(8, arr.footprint(), arr.len());
+    println!("bench_get_100K n:{} mem_ratio:{}%", arr.len(), ratio);
 }
 
 #[bench]
@@ -72,18 +72,18 @@ fn bench_get_100K(b: &mut Bencher) {
 fn bench_set_100K(b: &mut Bencher) {
     let seed: u128 = random();
     let mut rng = SmallRng::from_seed(seed.to_le_bytes());
-    let mut rr: Vector<u64> = Vector::new();
+    let mut arr: Vector<u64> = Vector::new();
     for _ in 0..100_000 {
-        let off = rng.gen::<usize>() % (rr.len() + 1);
-        rr = rr.insert(off, rng.gen::<u64>()).unwrap();
+        let off = rng.gen::<usize>() % (arr.len() + 1);
+        arr = arr.insert(off, rng.gen::<u64>()).unwrap();
     }
     b.iter(|| {
-        let off = rng.gen::<usize>() % rr.len();
-        rr = rr.set(off, rng.gen::<u64>()).unwrap();
+        let off = rng.gen::<usize>() % arr.len();
+        arr = arr.set(off, rng.gen::<u64>()).unwrap();
     });
 
-    let ratio = mem_ratio(8, rr.footprint(), rr.len());
-    println!("bench_set_100K n:{} mem_ratio:{}%", rr.len(), ratio);
+    let ratio = mem_ratio(8, arr.footprint(), arr.len());
+    println!("bench_set_100K n:{} mem_ratio:{}%", arr.len(), ratio);
 }
 
 #[bench]
@@ -93,19 +93,37 @@ fn bench_delete_100K(b: &mut Bencher) {
     // let seed: u128 = 165591759058987334402931296907057276118;
     println!("seed {}", seed);
     let mut rng = SmallRng::from_seed(seed.to_le_bytes());
-    let mut rr: Vector<u64> = Vector::new();
+    let mut arr: Vector<u64> = Vector::new();
     for _ in 0..100_000 {
-        let off = rng.gen::<usize>() % (rr.len() + 1);
-        rr = rr.insert(off, rng.gen::<u64>()).unwrap();
+        let off = rng.gen::<usize>() % (arr.len() + 1);
+        arr = arr.insert(off, rng.gen::<u64>()).unwrap();
     }
     b.iter(|| {
-        let off = rng.gen::<usize>() % rr.len();
-        rr = rr.delete(off).unwrap();
-        rr = rr.insert(off, rng.gen::<u64>()).unwrap();
+        let off = rng.gen::<usize>() % arr.len();
+        arr = arr.delete(off).unwrap();
+        arr = arr.insert(off, rng.gen::<u64>()).unwrap();
     });
 
-    let ratio = mem_ratio(8, rr.footprint(), rr.len());
-    println!("bench_delete_100K n:{} mem_ratio:{}%", rr.len(), ratio);
+    let ratio = mem_ratio(8, arr.footprint(), arr.len());
+    println!("bench_delete_100K n:{} mem_ratio:{}%", arr.len(), ratio);
+}
+
+#[bench]
+fn bench_clone(b: &mut Bencher) {
+    let seed: u128 = random();
+    // let seed: u128 = 165591759058987334402931296907057276118;
+    println!("seed {}", seed);
+    let mut rng = SmallRng::from_seed(seed.to_le_bytes());
+    let mut arr: Vector<u64> = Vector::new();
+    for _ in 0..100_000 {
+        let off = rng.gen::<usize>() % (arr.len() + 1);
+        arr = arr.insert(off, rng.gen::<u64>()).unwrap();
+    }
+    let mut n = 0;
+    b.iter(|| {
+        let arr1 = arr.clone();
+        n += arr1.len()
+    });
 }
 
 fn mem_ratio(size: usize, mem: usize, n: usize) -> f64 {
