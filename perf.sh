2e8027a65a8579dca038702ae52001cd440b40df
prataprc <prataprc@gmail.com>
2021-08-31 16:43:50 +0530

    improvise check.sh and perf.sh

--- perf.sh
+++ perf.sh
@@ -1,14 +1,19 @@
 #! /usr/bin/env bash
 
-exec > perf.out
+exec > $1
 exec 2>&1
 
 set -o xtrace
 
 PERF=$HOME/.cargo/target/release/perf
 
+# regular benchmark
 date; time cargo +nightly bench -- --nocapture || exit $?
 date; time cargo +stable bench -- --nocapture || exit $?
 
-date; time cargo +nightly run --release --bin perf --features=perf -- --loads 100000 --ops 10000 || exit $?
-date; valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes $PERF --loads 10000 --ops 10000 || exit $?
+LOADS=100000
+OPS=10000
+# invoke perf binary
+date; time cargo +nightly run --release --bin perf --features=perf -- --loads $LOADS --ops $OPS || exit $?
+# invoke perf binary, with valgrid
+date; valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes $PERF --loads 10000 --ops $OPS || exit $?
